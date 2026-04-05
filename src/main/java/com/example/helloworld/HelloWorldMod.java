f186d1a7e30213aba4dbe83898b5da76d3ddd4a0
前进老明 <siemenliu@gmail.com>
2026-04-05 15:08:45 +0800

    添加 /lze 回显命令

--- src/main/java/com/example/helloworld/HelloWorldMod.java
+++ src/main/java/com/example/helloworld/HelloWorldMod.java
@@ -1,7 +1,12 @@
 package com.example.helloworld;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
@@ -21,5 +26,20 @@ public class HelloWorldMod implements ModInitializer {
             ServerPlayerEntity player = handler.getPlayer();
             player.sendMessage(Text.literal("Hello World!"), false);
         });
+
+        // 注册 /echo 命令：将命令后的所有文字回显到聊天框
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("lze")
+                .then(CommandManager.argument("message", StringArgumentType.greedyString())
+                    .executes(this::executeEcho)
+                )
+            );
+        });
+    }
+
+    private int executeEcho(CommandContext<ServerCommandSource> context) {
+        String message = StringArgumentType.getString(context, "message");
+        context.getSource().sendFeedback(() -> Text.literal(message), false);
+        return 1;
     }
 }
